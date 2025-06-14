611fe41e982514b22d9f5668598bc1f05cada447
Timothy Julian <timothyjulian2001@yahoo.co.id>
2025-06-15 00:49:16 +0700

    multiple sender

--- src/channels.rs
+++ src/channels.rs
@@ -45,4 +45,36 @@ mod tests {
         let _ = handler1.join();
         let _ = handler2.join();
     }
+
+    #[test]
+    fn test_multiple_sender() {
+        let (sender, receiver) = mpsc::channel::<String>();
+        let sender2 = sender.clone();
+
+        let handler1 = thread::spawn(move || {
+            for i in 1..=5 {
+                thread::sleep(Duration::from_secs(2));
+                let _ = sender.send(format!("Hello from handler1 {}", i));
+            }
+            let _ = sender.send("Exit".to_string());
+        });
+
+        let handler1_add = thread::spawn(move || {
+            for i in 1..=5 {
+                thread::sleep(Duration::from_secs(2));
+                let _ = sender2.send(format!("Hello from handler1_add {}", i));
+            }
+            let _ = sender2.send("Exit".to_string());
+        });
+
+        let handler2 = thread::spawn(move || {
+            for value in receiver.iter() {
+                println!("{}", value);
+            }
+        });
+
+        let _ = handler1.join();
+        let _ = handler1_add.join();
+        let _ = handler2.join();
+    }
 }
