5ae2073b91a8c7fb76d2203570e560b2fdecedb4
Timothy Julian <timothyjulian2001@yahoo.co.id>
2025-06-14 18:34:48 +0700

    thread join, name, id, move

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,17 @@ fn main() {
 mod tests {
     use std::{thread, time::Duration};
 
+    fn calculate() -> i32 {
+        let mut counter = 0;
+        let current = thread::current();
+        for i in 1..=5 {
+            println!("[{}-{:?}] counter: {}",current.name().unwrap_or_default(), current.id(), i);
+            thread::sleep(Duration::from_secs(i));
+            counter += 1;
+        }
+        
+        counter
+    }
 
     #[test]
     fn test_create_thread() {
@@ -39,5 +50,41 @@ mod tests {
         }
 
         println!("application DONE");
+    }
+
+    #[test]
+    fn test_sequential() {
+        let result1 = calculate();
+        let result2 = calculate();
+
+        println!("total: {}", result1 + result2);
+        println!("application finished");
+    }
+
+    #[test]
+    fn test_parallel() {
+        let handle1 = thread::spawn(|| calculate());
+        let handle2 = thread::spawn(|| calculate());
+
+        let result1 = handle1.join().unwrap_or_default();
+        let result2 = handle2.join().unwrap_or_default();
+
+        println!("total: {}", result1 + result2);
+        println!("application finished");
+    }
+
+    #[test]
+    fn test_closure() {
+        let name = String::from("Timothy");
+        let closure = move || { //need to use move here to move the value to the closure scope
+            thread::sleep(Duration::from_secs(1));
+            println!("Hello, {}", name);
+        };
+        
+        let handler = thread::spawn(closure);
+        handler.join().unwrap();
+
+
+        // println!("{}", name);
     }
 }
